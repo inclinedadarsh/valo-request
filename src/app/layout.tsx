78b119c0360c331eb80cabed45fdc392864abe34
Adarsh Dubey <dubeyadarshmain@gmail.com>
2025-04-27 21:10:08 +0530

    feat: add clerk middleware and provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,11 @@
+import {
+	ClerkProvider,
+	SignInButton,
+	SignUpButton,
+	SignedIn,
+	SignedOut,
+	UserButton,
+} from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -23,12 +31,23 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body
+					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				>
+					<header className="flex justify-end items-center p-4 gap-4 h-16">
+						<SignedOut>
+							<SignInButton />
+							<SignUpButton />
+						</SignedOut>
+						<SignedIn>
+							<UserButton />
+						</SignedIn>
+					</header>
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
